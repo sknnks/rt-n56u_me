0a8b9057ed516a4113e05fc31e33a00142502d9b
nanchuci <71806013+nanchuci@users.noreply.github.com>
2020-11-20 11:41:26 +0800

    Add files via upload

--- trunk/configs/boards/BELL-A-040W-Q/board.h
+++ trunk/configs/boards/BELL-A-040W-Q/board.h
@@ -1,27 +1,27 @@
-/* ZTE_E8820s */
+/* MSG1500 */
 
-#define BOARD_PID		"ZTE_E8820s"
-#define BOARD_NAME		"ZTE_E8820s"
-#define BOARD_DESC		"ZTE_E8820s Wireless Router"
-#define BOARD_VENDOR_NAME	"ZTE Technology"
-#define BOARD_VENDOR_URL	"http://www.ztehome.com.cn/"
-#define BOARD_MODEL_URL		"http://www.ztehome.com.cn/"
+#define BOARD_PID		"BELL-A-040W-Q"
+#define BOARD_NAME		"BELL-A-040W-Q"
+#define BOARD_DESC		"nokia-sbell A-040W-Q Wireless Router"
+#define BOARD_VENDOR_NAME	"nokia-sbell"
+#define BOARD_VENDOR_URL	"http://www.nokia-sbell.com/"
+#define BOARD_MODEL_URL		"http://www.nokia-sbell.com/"
 #define BOARD_BOOT_TIME		30
-#define BOARD_FLASH_TIME   	120
-#define BOARD_GPIO_BTN_FN1    10
-#define BOARD_GPIO_BTN_RESET	18
-#define BOARD_GPIO_BTN_WPS	8
+#define BOARD_FLASH_TIME	120
+#undef BOARD_GPIO_BTN_FN1
+#define BOARD_GPIO_BTN_RESET 18
+#undef BOARD_GPIO_BTN_WPS
 #undef  BOARD_GPIO_LED_ALL
-#undef BOARD_GPIO_LED_WIFI  
-#undef BOARD_GPIO_LED_SW2G   
-#undef BOARD_GPIO_LED_SW5G   
-#define BOARD_GPIO_LED_POWER	3
-#define BOARD_GPIO_LED_SYS	16
-#undef BOARD_GPIO_LED_LAN
-#undef BOARD_GPIO_LED_WAN	
-#undef BOARD_GPIO_LED_USB	
+#undef  BOARD_GPIO_LED_WIFI
+#define  BOARD_GPIO_LED_SW2G   4
+#define  BOARD_GPIO_LED_SW5G  3
+#undef  BOARD_GPIO_LED_POWER
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN	15
+#define BOARD_GPIO_LED_USB  13
 #undef BOARD_GPIO_LED_ROUTER
-#undef  BOARD_GPIO_PWR_USB
+#undef BOARD_GPIO_PWR_USB_ON
+#undef BOARD_GPIO_PWR_USB
 #define BOARD_HAS_5G_11AC	1
 #define BOARD_NUM_ANT_5G_TX	2
 #define BOARD_NUM_ANT_5G_RX	2
@@ -30,7 +30,5 @@
 #define BOARD_NUM_ETH_LEDS	1
 #define BOARD_HAS_EPHY_L1000	1
 #define BOARD_HAS_EPHY_W1000	1
-#define BOARD_NUM_UPHY_USB3	1
-#define BOARD_GPIO_PWR_USB	11
-#define BOARD_GPIO_PWR_USB_ON	1
-#define BOARD_USB_PORT_SWAP	0
+#undef BOARD_NUM_UPHY_USB3
+#define BOARD_USB_PORT_SWAP	1
