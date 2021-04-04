a6454b654221acaf7716599ead67b0c3d4839870
panybbib <pan.107@163.com>
2021-04-04 12:45:20 +0800

    Update p910nd.c

--- trunk/user/p910nd/p910nd.c
+++ trunk/user/p910nd/p910nd.c
@@ -544,7 +544,7 @@ void server(int lpnumber)
 	struct sockaddr_storage client;
 	struct addrinfo hints, *res, *ressave;
 	char pidfilename[sizeof(PIDFILE)];
-	char service[10];       // 9100 (65535 max)
+	char service[10];	// 9100 (65535 max)
 	FILE *f;
 	const int bufsiz = 65536;
 
