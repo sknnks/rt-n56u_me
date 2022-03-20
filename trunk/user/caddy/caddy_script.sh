69b7ca10c79ad9d3d6cbea769965d60eb08ba0a6
panybbib <pan.107@163.com>
2022-03-20 16:06:33 +0800

    Update caddy_script.sh

--- trunk/user/caddy/caddy_script.sh
+++ trunk/user/caddy/caddy_script.sh
@@ -1,22 +1,53 @@
 #!/bin/sh
+
 caddy_storage=`nvram get caddy_storage`
 caddy_dir=`nvram get caddy_dir`
+caddy_file=`nvram get caddy_file`
 caddyf_wan_port=`nvram get caddyf_wan_port`
+caddyw_wan_port=`nvram get caddyw_wan_port`
+caddy_wname=`nvram get caddy_wname`
+caddy_wpassword=`nvram get caddy_wpassword`
+caddyfile="$caddy_dir/caddy/caddyfile"
+rm -f $caddyfile
 
-caddybin="/usr/bin/filebrowser"
+if [ "$caddy_file" = "0" ] || [ "$caddy_file" = "2" ]; then
+cat <<-EOF >/tmp/cf
+	:$caddyf_wan_port {
+	root $caddy_storage
+	timeouts none
+	gzip
+	filebrowser / $caddy_storage {
+	database /etc/storage/caddy_filebrowser.db
+	}
+}
+EOF
+fi
+
+if [ "$caddy_file" = "1" ] || [ "$caddy_file" = "2" ]; then
+cat <<-EOF >/tmp/cw
+	:$caddyw_wan_port {
+	root $caddy_storage
+	timeouts none
+	browse
+	gzip
+	filebrowser /document $caddy_storage {
+	database /etc/storage/caddy_filebrowser.db
+	}
+	basicauth / $caddy_wname $caddy_wpassword
+	webdav /disk {
+	scope $caddy_storage
+	allow $caddy_storage 
+	}
+}
+EOF
+fi
+
+cat /tmp/cw /tmp/cf > $caddyfile
+rm -f /tmp/cw
+rm -f /tmp/cf
+caddybin="/usr/bin/caddy_filebrowser"
+
 if [ ! -f "$caddybin" ]; then
-caddybin="$caddy_dir/caddy/filebrowser"
+	caddybin="$caddy_dir/caddy/caddy_filebrowser"
 fi
-
-if [ ! -e /etc/storage/filebrowser.db ]; then
- $caddybin -d /etc/storage/filebrowser.db config init
- $caddybin -d /etc/storage/filebrowser.db users add admin admin --perm.admin
- $caddybin -d /etc/storage/filebrowser.db config set --address 0.0.0.0 --locale zh-cn
- [ ! -d "$caddy_dir/caddy/cache" ] && mkdir -p 777 "$caddy_dir/caddy/cache"
-fi
-
-$caddybin -d /etc/storage/filebrowser.db config set \
- --port $caddyf_wan_port \
- --root $caddy_storage
- 
-$caddybin -d /etc/storage/filebrowser.db --disable-preview-resize --disable-type-detection-by-header --cache-dir $caddy_dir/caddy/cache &
+$caddybin -conf $caddyfile &
