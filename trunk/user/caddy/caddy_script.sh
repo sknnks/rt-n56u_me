9c0ae73490faa4b443c890d23ee9e83f7034a884
panybbib <pan.107@163.com>
2022-03-20 08:32:33 +0800

    Update caddy_script.sh

--- trunk/user/caddy/caddy_script.sh
+++ trunk/user/caddy/caddy_script.sh
@@ -12,7 +12,7 @@ if [ ! -e /etc/storage/filebrowser.db ]; then
  $caddybin -d /etc/storage/filebrowser.db config init
  $caddybin -d /etc/storage/filebrowser.db users add admin admin --perm.admin
  $caddybin -d /etc/storage/filebrowser.db config set --address 0.0.0.0 --locale zh-cn
- [ ! -d "$caddy_dir/caddy/cache" ] && mkdir 777 "$caddy_dir/caddy/cache"
+ [ ! -d "$caddy_dir/caddy/cache" ] && mkdir -p 777 "$caddy_dir/caddy/cache"
 fi
 
 $caddybin -d /etc/storage/filebrowser.db config set \
