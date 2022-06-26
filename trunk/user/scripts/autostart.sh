8687838a1b62275ebe7fcc83c99c94a889a0d6ec
panybbib <pan.107@163.com>
2022-06-26 23:02:25 +0800

    update
    update

--- trunk/user/scripts/autostart.sh
+++ trunk/user/scripts/autostart.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #nvram set ntp_ready=0
 
-mkdir -p /tmp/dnsmasq.dom
-logger -t "为防止dnsmasq启动失败，创建/tmp/dnsmasq.dom/"
+mkdir -p /tmp/dnsmasq.music
+logger -t "为防止dnsmasq启动失败，创建/tmp/dnsmasq.music"
 
 if [ $(nvram get sdns_enable) = 1 ] ; then
 logger -t "自动启动" "正在启动SmartDns"
