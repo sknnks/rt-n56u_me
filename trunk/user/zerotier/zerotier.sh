d92e3b3c1439c949adbf34362aaf81936ab67b48
panybbib <pan.107@163.com>
2022-07-22 07:19:29 +0800

    zerotier

--- trunk/user/zerotier/zerotier.sh
+++ trunk/user/zerotier/zerotier.sh
@@ -40,24 +40,21 @@ start_instance() {
 		$PROGIDT getpublic $config_path/identity.secret >$config_path/identity.public
 	fi
 
-	add_join $(nvram get zerotier_id)
-
+	add_join "$(nvram get zerotier_id)"
 	$PROG $args $config_path >/dev/null 2>&1 &
-
 	rules
 
 	if [ -n "$moonid" ]; then
 		$PROGCLI -D$config_path orbit $moonid $moonid
-		logger -t "zerotier" "orbit moonid $moonid ok!"
+		logger -t "zerotier" "orbit moonid $moonid OK!"
 	fi
-
 
 	if [ -n "$enablemoonserv" ]; then
 		if [ "$enablemoonserv" -eq "1" ]; then
 			logger -t "zerotier" "creat moon start"
 			creat_moon
 		else
-			logger -t "zerotier" "remove moon start"
+			logger -t "zerotier" "moon start cancelled"
 			remove_moon
 		fi
 	fi
@@ -69,56 +66,51 @@ add_join() {
 		touch $config_path/networks.d/$1.conf
 }
 
-
 rules() {
 	while [ "$(ifconfig | grep zt | awk '{print $1}')" = "" ]; do
 		sleep 1
 	done
-	nat_enable=$(nvram get zerotier_nat)
-	zt0=$(ifconfig | grep zt | awk '{print $1}')
+	zt0="$(ifconfig | grep zt | awk '{print $1}')"
 	logger -t "zerotier" "zt interface $zt0 is started!"
 	del_rules
-	iptables -A INPUT -i $zt0 -j ACCEPT
-	iptables -A FORWARD -i $zt0 -o $zt0 -j ACCEPT
-	iptables -A FORWARD -i $zt0 -j ACCEPT
-	if [ $nat_enable -eq 1 ]; then
-		iptables -t nat -A POSTROUTING -o $zt0 -j MASQUERADE
-		while [ "$(ip route | grep "dev $zt0  proto" | awk '{print $1}')" = "" ]; do
-			sleep 1
-	    done
-		ip_segment=`ip route | grep "dev $zt0  proto" | awk '{print $1}'`
-		iptables -t nat -A POSTROUTING -s $ip_segment -j MASQUERADE
+	iptables -A INPUT -i "$zt0" -j ACCEPT
+	iptables -A FORWARD -i "$zt0" -o "$zt0" -j ACCEPT
+	iptables -A FORWARD -i "$zt0" -j ACCEPT
+	if [ "$(nvram get zerotier_nat)" -eq 1 ]; then
+		iptables -t nat -A POSTROUTING -o "$zt0" -j MASQUERADE
+		ip_segment="$(ip route | grep "dev $zt0  proto kernel" | awk '{print $1}')"
+		iptables -t nat -A POSTROUTING -s "${ip_segment}" -j MASQUERADE
 		zero_route "add"
 	fi
-
 }
 
 del_rules() {
-	zt0=$(ifconfig | grep zt | awk '{print $1}')
-	ip_segment=`ip route | grep "dev $zt0  proto" | awk '{print $1}'`
-	iptables -D FORWARD -i $zt0 -j ACCEPT 2>/dev/null
-	iptables -D FORWARD -o $zt0 -j ACCEPT 2>/dev/null
-	iptables -D FORWARD -i $zt0 -o $zt0 -j ACCEPT
-	iptables -D INPUT -i $zt0 -j ACCEPT 2>/dev/null
-	iptables -t nat -D POSTROUTING -o $zt0 -j MASQUERADE 2>/dev/null
-	iptables -t nat -D POSTROUTING -s $ip_segment -j MASQUERADE 2>/dev/null
+	zt0="$(ifconfig | grep zt | awk '{print $1}')"
+	ip_segment="$(ip route | grep "dev $zt0  proto kernel" | awk '{print $1}')"
+	iptables -D FORWARD -i "$zt0" -j ACCEPT 2>/dev/null
+	iptables -D FORWARD -o "$zt0" -j ACCEPT 2>/dev/null
+	iptables -D FORWARD -i "$zt0" -o "$zt0" -j ACCEPT 2>/dev/null
+	iptables -D INPUT -i "$zt0" -j ACCEPT 2>/dev/null
+	iptables -t nat -D POSTROUTING -o "$zt0" -j MASQUERADE 2>/dev/null
+	iptables -t nat -D POSTROUTING -s "${ip_segment}" -j MASQUERADE 2>/dev/null
 }
 
 zero_route(){
+	zt0="$(ifconfig | grep zt | awk '{print $1}')"
 	rulesnum=`nvram get zero_staticnum_x`
 	for i in $(seq 1 $rulesnum)
 	do
-		j=`expr $i - 1`
-		route_enable=`nvram get zero_enable_x$j`
-		zero_ip=`nvram get zero_ip_x$j`
-		zero_route=`nvram get zero_route_x$j`
+		j="`expr $i - 1`"
+		route_enable="`nvram get zero_enable_x$j`"
+		zero_ip="`nvram get zero_ip_x$j`"
+		zero_route="`nvram get zero_route_x$j`"
 		if [ "$1" = "add" ]; then
-			if [ $route_enable -ne 0 ]; then
-				ip route add $zero_ip via $zero_route dev $zt0
+			if [ "$route_enable" -ne 0 ]; then
+				ip route add "$zero_ip" via "$zero_route" dev "$zt0"
 				echo "$zt0"
 			fi
 		else
-			ip route del $zero_ip via $zero_route dev $zt0
+			ip route del "$zero_ip" via "$zero_route" dev "$zt0"
 		fi
 	done
 }
@@ -131,7 +123,7 @@ start_zero() {
 
 kill_z() {
 	killall -9 zerotier-one >/dev/null 2>&1
-	zerotier_process=$(pidof zerotier-one)
+	zerotier_process="$(pidof zerotier-one)"
 	if [ -n "$zerotier_process" ]; then
 		kill -9 "$zerotier_process" >/dev/null 2>&1
 	fi
@@ -151,22 +143,22 @@ creat_moon(){
 	logger -t "zerotier" "moonip $moonip"
 	#检查是否合法ip
 	regex="\b(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[1-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[1-9])\b"
-	ckStep2=`echo $moonip | egrep $regex | wc -l`
+	ckStep2="`echo $moonip | egrep $regex | wc -l`"
 
 	logger -t "zerotier" "搭建ZeroTier的Moon中转服务器，生成moon配置文件"
 	if [ -z "$moonip" ]; then
 		#自动获取wanip
-		ip_addr=`ifconfig -a ppp0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
-	#elif [ $ckStep2 -eq 0 ]; then
+		ip_addr="`ifconfig -a ppp0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`"
+	#elif [ "$ckStep2" -eq 0 ]; then
 		#不是ip
-	#	ip_addr = `curl $moonip`
+	#	ip_addr ="`curl $moonip`"
 	else
-		ip_addr=$moonip
+		ip_addr="$moonip"
 	fi
 	logger -t "zerotier" "moonip $ip_addr"
-	if [ -e $config_path/identity.public ]; then
+	if [ -e "$config_path/identity.public" ]; then
 
-		$PROGIDT initmoon $config_path/identity.public > $config_path/moon.json
+		$PROGIDT initmoon "$config_path/identity.public" > "$config_path/moon.json"
 		if `sed -i "s/\[\]/\[ \"$ip_addr\/9993\" \]/" $config_path/moon.json >/dev/null 2>/dev/null`; then
 			logger -t "zerotier" "生成moon配置文件成功"
 		else
@@ -176,20 +168,18 @@ creat_moon(){
 		logger -t "zerotier" "生成签名文件"
 		cd $config_path
 		pwd
-		$PROGIDT genmoon $config_path/moon.json
+		$PROGIDT genmoon "$config_path/moon.json"
 		[ $? -ne 0 ] && return 1
 		logger -t "zerotier" "创建moons.d文件夹，并把签名文件移动到文件夹内"
-		if [ ! -d "$config_path/moons.d" ]; then
-			mkdir -p $config_path/moons.d
-		fi
+		[ -d "$config_path/moons.d" ] || mkdir -p "$config_path/moons.d"
 		
 		#服务器加入moon server
-		mv $config_path/*.moon $config_path/moons.d/ >/dev/null 2>&1
+		mv "$config_path/*.moon" "$config_path/moons.d/" >/dev/null 2>&1
 		logger -t "zerotier" "moon节点创建完成"
 
-		zmoonid=`cat moon.json | awk -F "[id]" '/"id"/{print$0}'` >/dev/null 2>&1
-		zmoonid=`echo $zmoonid | awk -F "[:]" '/"id"/{print$2}'` >/dev/null 2>&1
-		zmoonid=`echo $zmoonid | tr -d '"|,'`
+		zmoonid="`cat moon.json | awk -F "[id]" '/"id"/{print$0}'`" >/dev/null 2>&1
+		zmoonid="`echo $zmoonid | awk -F "[:]" '/"id"/{print$2}'`" >/dev/null 2>&1
+		zmoonid="`echo $zmoonid | tr -d '"|,'`" >/dev/null 2>&1
 
 		nvram set zerotiermoon_id="$zmoonid"
 		nvram commit
