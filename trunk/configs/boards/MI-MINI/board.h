189da8ab62a6e3896051c3867d898c9b485930b2
panybbib <pan.107@163.com>
2022-08-15 23:55:45 +0800

    Update board.h

--- trunk/configs/boards/MI-MINI/board.h
+++ trunk/configs/boards/MI-MINI/board.h
@@ -6,19 +6,19 @@
 #define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
 #define BOARD_VENDOR_URL	"http://www.mi.com/"
 #define BOARD_MODEL_URL		"http://www.mi.com/miwifimini"
-#define BOARD_BOOT_TIME		25
+#define BOARD_BOOT_TIME		30
 #define BOARD_FLASH_TIME	120
 #define BOARD_GPIO_BTN_RESET	30
 #undef  BOARD_GPIO_BTN_WPS
 #undef  BOARD_GPIO_LED_ALL
 #undef  BOARD_GPIO_LED_WIFI
 #define BOARD_GPIO_LED_POWER	24	/* 24: blue, 26: yellow, 29: red */
-#define BOARD_GPIO_LED_LAN	29
 #undef  BOARD_GPIO_LED_WAN
+#define BOARD_GPIO_LED_LAN	29
 #define BOARD_GPIO_LED_USB	26
 #undef  BOARD_GPIO_LED_ROUTER
-#define BOARD_GPIO_PWR_USB	65
 #define BOARD_GPIO_PWR_USB_ON	1	/* 1: 5V Power ON, 0: 5V Power OFF */
+#define BOARD_GPIO_PWR_USB	65
 #define BOARD_HAS_5G_11AC	1
 #define BOARD_NUM_ANT_5G_TX	2
 #define BOARD_NUM_ANT_5G_RX	2
