acddedaacc0d83c32a4cf3c9325dff79205afa05
panybbib <pan.107@163.com>
2023-03-13 01:02:19 +0800

    Update dnsmasq.sh

--- trunk/user/dnsmasq/dnsmasq.sh
+++ trunk/user/dnsmasq/dnsmasq.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-filter_aaaa=`nvram get dhcp_filter_aaa`
-
-if [ $filter_aaaa = 0 ]; then
-sed -i '/filter-aaaa/d' /etc/storage/dnsmasq/dnsmasq.conf
- else 
-sed -i '/filter-aaaa/d' /etc/storage/dnsmasq/dnsmasq.conf
-	cat >>/etc/storage/dnsmasq/dnsmasq.conf <<EOF
-filter-aaaa
-EOF
-fi
+#filter_aaaa=`nvram get dhcp_filter_aaaa`
+#if [ $filter_aaaa = 0 ]; then
+#sed -i '/filter-aaaa/d' /etc/storage/dnsmasq/dnsmasq.conf
+#else 
+#sed -i '/filter-aaaa/d' /etc/storage/dnsmasq/dnsmasq.conf
+#cat >>/etc/storage/dnsmasq/dnsmasq.conf <<EOF
+#filter-aaaa
+#EOF
+#fi
